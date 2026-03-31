18a8bd00cddc5d8b8601789fc1c847ee30edcbac
Rashedul2001 <hacker360sr@gmail.com>
2026-03-31 19:02:44 +0600

    Add Container padding,fixes for minor details

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,7 +5,7 @@ import "../App.css"
 
 const Banner = () => {
     return (
-        <div className='flex flex-col lg:flex-row mx-auto w-10/12 my-10 lg:my-16 lg:gap-8 gap-4'>
+        <div className='p-5 flex flex-col lg:flex-row container mx-auto  my-10 lg:my-16 lg:gap-8 gap-4'>
             <div className='flex-3 space-y-4 '>
                 <p className='inline-flex gap-1 rounded-full px-3 py-1 font-medium text-purple-500 bg-blue-100' >
                     <img src={circle} alt="ai" className='inline' />
@@ -16,8 +16,8 @@ const Banner = () => {
                 </p>
 
                 <div className="items-center flex gap-4">
-                    <button className='blue-to-purple-linear p-3 rounded-full text-white'>Explore Products</button>
-                    <button className="inline-flex p-3 border border-purple-500 text-purple-500 rounded-full "><img src={playIcon} alt="Play" className="mr-2" /> Watch Demo</button>
+                    <button className='blue-to-purple-linear p-3 rounded-full text-white font-bold'>Explore Products</button>
+                    <button className="inline-flex p-3 border border-purple-500 text-purple-500 rounded-full font-bold "><img src={playIcon} alt="Play" className="mr-2" /> Watch Demo</button>
                 </div>
             </div>
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -14,14 +14,14 @@ const NavBar = () => {
 
 
     return (
-        <nav className='flex justify-around items-center h-12 '>
+        <nav className='p-5 flex justify-between items-center h-12 container mx-auto '>
             <div className='flex items-center gap-2'>
                 <span onClick={() => setMobileMenuStatus(!mobileMenuStatus)}>
                     {
                         mobileMenuStatus ? <GiHamburgerMenu className="lg:hidden" /> : <IoMdClose className='lg:hidden' />
                     }
                 </span>
-                <a className="font-bold cursor-pointer text-3xl blue-to-purple-linear bg-clip-text text-transparent">DigiTools</a>
+                <a className="font-extrabold  cursor-pointer text-3xl blue-to-purple-linear bg-clip-text text-transparent">DigiTools</a>
                 <ul className={` absolute duration-1000 lg:hidden  ${mobileMenuStatus ? " -top-40" : "top-12"} `}>
                     <li><a>Products</a></li>
                     <li><a>Features</a></li>
@@ -29,8 +29,6 @@ const NavBar = () => {
                     <li><a>Testimonials</a></li>
                     <li><a>FAQ</a></li>
                 </ul>
-
-
             </div>
             <div>
                 <ul className='lg:flex items-center hidden '>
@@ -44,7 +42,7 @@ const NavBar = () => {
             <div className='space-x-4 flex items-center'>
                 <a className=''><img src={cartIcon} alt="shopping cart" /></a>
                 <a className='font-semibold'>Login</a>
-                <button className="blue-to-purple-linear py-1 px-3  rounded-full font-semibold whitespace-nowrap text-white cursor-pointer" >Get Started</button>
+                <button className="blue-to-purple-linear py-2 px-3  rounded-full font-semibold whitespace-nowrap text-white cursor-pointer" >Get Started</button>
             </div>
 
         </nav>
